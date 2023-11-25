9145f7e030b498224c11060781d6cfefbb4c7a22
Abel214 <abelmoral@gmail.com>
2023-11-25 15:13:30 -0500

    Subida clase img

--- Tareas/src/tareas/Tareas.java
+++ Tareas/src/tareas/Tareas.java
@@ -10,11 +10,12 @@ package tareas;
  */
 public class Tareas {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        
+//        
+//    }
     
 }
